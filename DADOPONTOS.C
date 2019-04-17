b9a26149465b7158d33d8684b24090c02ab3c8d6
kaliljoao <joao.lyra@morningstar.com.br>
2019-04-17 18:11:20 -0300

    inicialização do módulo DADOPONTOS.C

--- DADOPONTOS.C
+++ DADOPONTOS.C
@@ -1,16 +1,29 @@
+#pragma once
 /***************************************************************************
 *  $MCI Módulo de implementação: Módulo Dado Pontos
 *
 *  Arquivo gerado:              DADOPONTOS.C
-*  Letras identificadoras:      DPT
+*  Letras identificadoras:      DDPnt
 *
 *  Projeto: Disciplina INF 1301
 *  Autores: elu - Emanuel Lima Umbelino
-*        jpk - João Pedro Kalil
-*        phs - Pedro Henrique Soares
+*			jpk - João Pedro Kalil
+*			phs - Pedro Henrique Soares
 *
 *  $HA Histórico de evolução:
 *     Versão   Autores	  Data           Observações
 *      1.00    elu	  	17/04/2019     Inicializado projeto
+*	   1.00    jpk	  	17/04/2019     Criação dos escopos das funções
 *
 ***************************************************************************/
+
+#include "DADOPONTOS.H"
+#include <stdlib.h>
+#include <stdio.h> 
+
+typedef struct tgDadoPontos
+{
+	
+	int valor;
+
+} tppDadoPontos ;
