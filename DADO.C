5c61ca8d89482739d560b85b5d3da8f0ffbe18a1
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2019-04-17 10:35:53 -0300

    Criados arquivos para entrega e comentarios iniciais

---
+++ DADO.C
@@ -0,0 +1,16 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo Dado
+*
+*  Arquivo gerado:              DADO.C
+*  Letras identificadoras:      DAD
+*
+*  Projeto: Disciplina INF 1301
+*  Autores: elu - Emanuel Lima Umbelino
+*        jpk - João Pedro Kalil
+*        phs - Pedro Henrique Soares
+*
+*  $HA Histórico de evolução:
+*     Versão   Autores	  Data           Observações
+*      1.00    elu	  	17/04/2019     Inicializado projeto
+*
+***************************************************************************/
