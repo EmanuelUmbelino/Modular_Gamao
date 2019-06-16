6f00d7840e0416095a9be4ebfdd95213d72a1f85
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2019-06-16 20:57:27 -0300

    Revisao módulo pecas capturadas

--- PecasCapturadas/PECASCAPTURADAS.C
+++ PecasCapturadas/PECASCAPTURADAS.C
@@ -1,7 +1,7 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: Módulo BAR
+*  $MCI Módulo de implementação: Módulo Peças Capturadas
 *
-*  Arquivo gerado:              BAR.C
+*  Arquivo gerado:              PECASCAPTURADAS.C
 *  Letras identificadoras:      BAR
 *
 *  Projeto: INF 1301 Jogo Gamão via Terminal
@@ -10,24 +10,23 @@
 *			phs - Pedro Henrique Soares
 *
 *  $HA Histórico de evolução:
-*    Versão	  Autores	 	   Data           Observações 
-*      2		jpk	  		04/06/2019     -Conserto da função de destruir a barra 
-*										   -Subtituição dos valores 0 e 1 por "Preta" e "Vermelha"
-*										   -Implementação da função de contar número de peças das listas da barra
-*
-*      1		jpk	  		24/04/2019     Criação do módulo
+*	Versão		Autores		Data			Observações
+*	3			elu			16/06/2019		Revisão Final
+*	2			jpk			04/06/2019		-Conserto da função de destruir a barra 
+*											-Subtituição dos valores 0 e 1 por "Preta" e "Vermelha"
+*											-Implementação da função de contar número de peças das listas da barra
+*	1			jpk			24/04/2019		Criação do módulo
 *
 ***************************************************************************/
 
-#include	<malloc.h>
-#include 	<stdio.h>
-#include	<stdlib.h>
-#include "..\Peca\PECA.H"
-#include "..\Lista\LISTA.H"
-#define BAR_OWN
-#include "PECASCAPTURADAS.H"
-#undef BAR_OWN
-
+	#include	<malloc.h>
+	#include 	<stdio.h>
+	#include	<stdlib.h>
+	#include	"..\Peca\PECA.H"
+	#include	"..\Lista\LISTA.H"
+	#define PECASCAPTURADAS_OWN
+	#include "PECASCAPTURADAS.H"
+	#undef PECASCAPTURADAS_OWN
 
 /***********************************************************************
 *
@@ -40,166 +39,158 @@
 ***********************************************************************/
 
 	typedef struct tgBarra {
-		LIS_tppLista Vermelhas;
-		LIS_tppLista Pretas;
+		LIS_tppLista Vermelhas ;
+		LIS_tppLista Pretas ;
 		/* Ponteiros para as lista de peças capturadas*/
 
-	} tpBarra;
-
+	} tpBarra ;
 
 /*****  Dados encapsulados no módulo  *****/
 
-	static tpBarra* bar = NULL;
+	static tpBarra* bar = NULL ;
 		/* Ponteiro para a barra de peças capturadas */
-
-
-
 		
 /*****  Código das funções exportadas pelo módulo  *****/
 
 /***************************************************************************
 *
 *  Função: BAR Criar Barra de peças capturadas
-*
-***************************************************************************/
+*  ****/
 
-	BAR_tpCondRet BAR_CriarBarra(void)
+	BAR_tpCondRet BAR_CriarBarra( void )
 	{
-		int i;
-		LIS_tppLista* casa = NULL;
-		bar = (tpBarra*)malloc(sizeof(tpBarra));
-		if (bar == NULL)
-		{
-			return BAR_CondRetFaltouMemoria;
+		LIS_tppLista* casa = NULL ;
+		int i ;
+
+		if ( bar != NULL ) {
+			BAR_DestruirBarra( ) ;
+		} /* if */
+
+		bar = ( tpBarra * ) malloc( sizeof( tpBarra ) ) ;
+		if ( bar == NULL ) {
+			return BAR_CondRetFaltouMemoria ;
 		} /* if */
 		
-		(bar)->Vermelhas = LIS_CriarLista(PEC_DestruirPeca);
-		(bar)->Pretas = LIS_CriarLista(PEC_DestruirPeca);
-		if ((bar)->Vermelhas == NULL || (bar)->Pretas == NULL)
-		{
-			return BAR_CondRetFaltouMemoria;
+		( bar )->Vermelhas = LIS_CriarLista(PEC_DestruirPeca) ;
+		( bar )->Pretas = LIS_CriarLista(PEC_DestruirPeca) ;
+		if ( ( bar )->Vermelhas == NULL || ( bar )->Pretas == NULL ) {
+			return BAR_CondRetFaltouMemoria ;
 		} /* if */
-		return BAR_CondRetOK;
-	}/* Fim função: BAR Criar Barra de peças capturadas */
 
+		return BAR_CondRetOK ;
+	}/* Fim função: BAR Criar Barra de peças capturadas */
 
 /***************************************************************************
 *
 *  Função: BAR Destruir Barra de peças capturadas
-*
-***************************************************************************/
+*  ****/
 
-	void BAR_DestruirBarra(void)
+	void BAR_DestruirBarra( void )
 	{
-		int i , numCasas;
-		LIS_tppLista* capturadas;
-		if (bar == NULL)
-		{
-			return;
+		LIS_tppLista * capturadas ;
+		int i, numCasas ;
+
+		if (bar == NULL) {
+			return ;
 		} /* if */
 
-		LIS_EsvaziarLista(bar->Vermelhas);
-		LIS_EsvaziarLista(bar->Pretas);
-		LIS_DestruirLista(bar->Vermelhas);
-		LIS_DestruirLista(bar->Pretas);
-		free(bar);
-		bar = NULL;
+		LIS_EsvaziarLista( bar->Vermelhas ) ;
+		LIS_DestruirLista( bar->Vermelhas ) ;
+
+		LIS_EsvaziarLista( bar->Pretas ) ;
+		LIS_DestruirLista( bar->Pretas ) ;
+
+		free( bar ) ;
+		bar = NULL ;
 	} /* Fim função: BAR Destruir Barra de peças capturadas */
 
 
 /***************************************************************************
 *
 *  Função: BAR Inserir peça na Barra de peças capturadas
-*
-***************************************************************************/
+*  ****/
 
-	BAR_tpCondRet BAR_InserePeca(CorPecas cor)
+	BAR_tpCondRet BAR_InserePeca( CorPecas cor )
 	{
-		PEC* peca;
+		LIS_tpCondRet cond ;
+		PEC* peca ;
 
-		if (bar == NULL) 
-		{
-			return BAR_CondRetBarNaoExiste;
+		if ( bar == NULL ) {
+			return BAR_CondRetBarNaoExiste ;
 		} /* if */
 
-		PEC_CriarPeca(&peca, cor);
-		if (peca == NULL) 
-		{
+		PEC_CriarPeca( &peca, cor ) ;
+		if ( peca == NULL ) {
 			return BAR_CondRetFaltouMemoria;
 		} /* if */
 		
-		if (cor == Preta)   // peças pretas
-		{
-			LIS_InserirElementoAntes((bar)->Pretas, peca);
-			return BAR_CondRetOK;
+		if (cor == Preta) {
+			cond = LIS_InserirElementoAntes( ( bar )->Pretas, peca ) ;
 		} /* if */
-		else if (cor == Vermelha)  // peças vermelhas
-		{
-			LIS_InserirElementoAntes((bar)->Vermelhas, peca);
-			return BAR_CondRetOK;
+		else {
+			cond = LIS_InserirElementoAntes( ( bar )->Vermelhas, peca ) ;
 		} /* else if */
+
+		if ( cond == LIS_CondRetFaltouMemoria ) {
+			return BAR_CondRetFaltouMemoria ;
+		} /* if */
+
+		return BAR_CondRetOK ;
 
 	}/* Fim função: BAR Inserir peça na Barra de peças capturadas */
 
 /***************************************************************************
 *
 *  Função: BAR Remover peça na Barra de peças capturadas
-*
-***************************************************************************/
+*  ****/
 
-	BAR_tpCondRet BAR_RemovePeca(CorPecas cor)
+	BAR_tpCondRet BAR_RemovePeca( CorPecas cor )
 	{
-		int cond;
+		int cond ;
 		
-		if (bar == NULL)
-		{
-			return BAR_CondRetBarNaoExiste;
+		if ( bar == NULL ) {
+			return BAR_CondRetBarNaoExiste ;
 		} /* if */
 
-		if (cor == Preta)
-		{
-			cond = LIS_ExcluirElemento((bar)->Pretas);
+		if ( cor == Preta ) {
+			cond = LIS_ExcluirElemento( ( bar )->Pretas ) ;
 		} /* if */
-		else if (cor == Vermelha)
-		{
-			cond = LIS_ExcluirElemento((bar)->Vermelhas);
+		else if ( cor == Vermelha ) {
+			cond = LIS_ExcluirElemento( ( bar )->Vermelhas ) ;
 		} /* else if */
 		
-		
-		if (cond == LIS_CondRetListaVazia) {
-			return BAR_CondRetListaVazia;
+		if ( cond == LIS_CondRetListaVazia ) {
+			return BAR_CondRetListaVazia ;
 		} /* if */
-		return BAR_CondRetOK;
+
+		return BAR_CondRetOK ;
 	} /* Fim função: BAR Remover peça da Barra de peças capturadas */
 
 /***************************************************************************
 *
-*  Função: BAR Numero de peças das listas da barra
-*
-***************************************************************************/
+*  Função: BAR Numero de peças da barra
+*  ****/
 
 	BAR_tpCondRet BAR_NumPecas( CorPecas cor, int * numPecas )
 	{
-		if (bar == NULL)
-		{
-			return BAR_CondRetBarNaoExiste;
+		if ( bar == NULL ) {
+			return BAR_CondRetBarNaoExiste ;
 		} /* if */
 
-		if (cor == Vermelha) 
-		{
-			if (bar->Vermelhas == NULL) 
-			{
-				return BAR_CondRetListaVazia;
+		if ( cor == Vermelha ) {
+			if ( bar->Vermelhas == NULL ) {
+				return BAR_CondRetListaVazia ;
 			} /* if */
-			LIS_NumElemenLista( bar->Vermelhas, numPecas );
+			LIS_NumElemenLista( bar->Vermelhas, numPecas ) ;
 		} /* if */
-		else 
-		{
-			if (bar->Pretas == NULL) 
-			{
-				return BAR_CondRetListaVazia;
+		else {
+			if ( bar->Pretas == NULL ) {
+				return BAR_CondRetListaVazia ;
 			} /* if */
-			LIS_NumElemenLista( bar->Pretas, numPecas );
+			LIS_NumElemenLista( bar->Pretas, numPecas ) ;
 		} /* else */
+
 		return BAR_CondRetOK;
-	} /* Fim função: BAR Numero de peças das listas da barra */
+	} /* Fim função: BAR Numero de peças da barra */
+
+/********** Fim do módulo de implementação: Módulo Tabuleiro **********/
--- PecasCapturadas/TESTBAR.C
+++ PecasCapturadas/TESTBAR.C
@@ -1,8 +1,8 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: Módulo de teste específico
+*  $MCI Módulo de implementação: Módulo de teste específico para Peças Capturadas
 *
 *  Arquivo gerado:              TESTBAR.C
-*  Letras identificadoras:      BAR
+*  Letras identificadoras:      TBAR
 *
 *  Projeto: INF 1301 Jogo Gamão via Terminal
 *  Autores: elu - Emanuel Lima Umbelino
@@ -10,58 +10,64 @@
 *           phs - Pedro Henrique Soares
 *
 *  $HA Histórico de evolução:
-*    Versão	  Autores		 	 Data        Observações
-*      1		jpk	  	03/06/2019     Inicializado projeto
+*	Versão		Autores		Data			Observações
+*	2			elu			16/06/2019		Revisão Final
+*	1			jpk			03/06/2019		Inicializado projeto
 *
 *  $ED Descrição do módulo
 *     Este módulo contém as funções específicas para o teste do
-*     módulo lista de peças capturadasa. Ilustra como redigir um interpretador 
+*     módulo peças capturadas. Ilustra como redigir um interpretador 
 *     de comandos de teste específicos utilizando o arcabouço de teste para C.
 *
 *  $EIU Interface com o usuário pessoa
-*     Comandos de teste específicos para testar o módulo tabuleiro:
+*     Comandos de teste específicos para testar o módulo peças capturadas:
 *
 *     "=criar"        - chama a função BAR_CriarBarra( )
+*
 *     "=insere <Int>"
 *                   - chama a função BAR_InserePecaLista( <Int> )
 *                     Obs. notação: <Int>  é o valor do parâmetro
 *                     que se encontra no comando de teste.
-*     "=destruir"   - chama a função BAR_DestruirBarra( )
+*
+*     "=obternum <CorPecas> <*Int>"
+*                   - chama a função BAR_NumPecas( <CorPecas> , <*Int> )
+*
 *     "=remover <Int> "
 *                   - chama a função BAR_RemovePecaLista( <Int> )
+*
+*     "=destruir"   - chama a função BAR_DestruirBarra( )
 *
 ***************************************************************************/
 
 #include    <string.h>
 #include    <stdio.h>
 
-#include    "TST_ESPC.H"
+#include    "..\TST_ESPC.H"
 
-#include    "GENERICO.H"
-#include    "LERPARM.H"
-#include    "LISTA.H"
-#include    "COR.H"
-#include    "PECA.H"
+#include    "..\GENERICO.H"
+#include    "..\LERPARM.H"
+#include    "..\Lista\LISTA.H"
+#include    "..\Cor\COR.H"
 #include    "PECASCAPTURADAS.H"
 
 /* Tabela dos nomes dos comandos de teste específicos */
 
 #define     CRIAR_FIM_CMD       "=criar"
 #define     INSERE_CMD         	"=insere"
+#define     OBTER_NUM_CMD       "=obternum"
 #define     REMOVE_CMD          "=remover"
 #define     DESTROI_CMD         "=destruir"
-#define     CONTA_PECAS_CMD     "=conta"
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
 
 /***********************************************************************
 *
-*  $FC Função: TBAR Efetuar operações de teste específicas para PECAS CAPTURADAS (BAR)
+*  $FC Função: TBAR Efetuar operações de teste específicas para pecas capturadas
 *
 *  $ED Descrição da função
 *     Efetua os diversos comandos de teste específicos para o módulo
-*     PECASCAPTURADAS ir sendo testado.
+*     pecas capturadas ir sendo testado.
 *
 *  $EP Parâmetros
 *     $P ComandoTeste - String contendo o comando
@@ -74,16 +80,25 @@
 	TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
 	{
 
-		BAR_tpCondRet CondRetObtido   = BAR_CondRetOK ;
-		BAR_tpCondRet CondRetEsperada = BAR_CondRetListaVazia;
-					/* inicializa para qualquer coisa */
+		BAR_tpCondRet CondRetObtido = BAR_CondRetOK ;
+		BAR_tpCondRet CondRetEsperada = BAR_CondRetListaVazia ;
+			/* inicializa com qualquer coisa */
 
-		CorPecas ValorRecebidoCor	= Neutro ;
-		int ValorRecebido			= 0 ;
-		int ValorEsperadoInt		= 0 ;
-		int ValorObtido				= 0 ;
-		int * NumPecas				= NULL;
-		int  NumLidos = -1 ;
+		CorPecas ValorRecebidoCor = Neutro ;
+		CorPecas ValorDadoCor = Neutro ;
+		CorPecas ValorEsperadoCor = Neutro ;
+		int ValorDadoInt = 0 ;
+		int ValorRecebidoInt = 0 ;
+		int ValorEsperadoInt = 0 ;
+
+
+		// CorPecas ValorRecebidoCor = Neutro ;
+		// int ValorRecebido = 0 ;
+		// int ValorEsperadoInt = 0 ;
+		// int ValorObtido = 0 ;
+		// int * NumPecas = NULL;
+
+		int NumLidos = -1 ;
 
 		TST_tpCondRet Ret ;
 
@@ -102,29 +117,55 @@
 				CondRetObtido = BAR_CriarBarra( ) ;
 
 				return TST_CompararInt( CondRetEsperada , CondRetObtido ,
-												"Retorno errado ao criar o barra de capturadas." ) ;
+												"Retorno errado ao criar a barra de capturadas." ) ;
 
 			} /* fim ativa: Testar BAR Criar Barra */
 
-		//* Testar BAR Insere Peça na Lista */
+		/* Testar BAR Insere Peça na Barra */
 
 			else if ( strcmp( ComandoTeste , INSERE_CMD ) == 0 )
 			{
 
 				NumLidos = LER_LerParametros( "ii" ,
-										&ValorRecebido , &CondRetEsperada ) ;
+										&ValorDadoCor , &CondRetEsperada ) ;
 				if ( NumLidos != 2 )
 				{
 					return TST_CondRetParm ;
 				} /* if */
 
+				CondRetObtido = BAR_InserePeca( ValorDadoCor ) ;
 
-				ValorRecebidoCor = (CorPecas)ValorRecebido;
-				CondRetObtido = BAR_InserePecaLista( (CorPecas)ValorRecebidoCor ) ;
 				return TST_CompararInt( CondRetEsperada , CondRetObtido ,
 												"Retorno errado ao inserir peça na casa dada." ) ;
 
 			} /* fim ativa: Testar BAR Insere Peça na Lista */
+
+		/* Testar BAR Numero de Peças na Lista */
+
+			else if ( strcmp( ComandoTeste , OBTER_NUM_CMD ) == 0 )
+			{
+
+				NumLidos = LER_LerParametros( "iii" ,
+										 &ValorDadoCor , &ValorEsperadoInt , &CondRetEsperada ) ;
+				if ( NumLidos != 3 )
+				{
+					return TST_CondRetParm ;
+				} /* if */
+
+				CondRetObtido = BAR_NumPecas( ValorDadoInt , &ValorRecebidoInt ) ;
+
+				Ret = TST_CompararInt( CondRetEsperada , CondRetObtido ,
+											  "Retorno errado ao obter número de peças da barra." ) ;
+
+				if ( Ret != TST_CondRetOK || CondRetObtido != BAR_CondRetOK )
+				{
+					return Ret ;
+				} /* if */
+
+				return TST_CompararInt( ValorEsperadoInt , ValorRecebidoInt ,
+												 "Quantidade de peças na barra está errada." ) ;
+
+			} /* fim ativa: Testar BAR Numero de Peças na Lista */
 
 		/* Testar BAR Remove Peça da Lista */
 
@@ -132,18 +173,16 @@
 			{
 
 				NumLidos = LER_LerParametros( "ii" ,
-										 &ValorRecebido , &CondRetEsperada ) ;
+										 &ValorDadoCor , &CondRetEsperada ) ;
 				if ( NumLidos != 2 )
 				{
 					return TST_CondRetParm ;
 				} /* if */
 
-				ValorRecebidoCor = (CorPecas)ValorRecebido;
-				CondRetObtido = BAR_RemovePecaLista( ValorRecebidoCor ) ;
+				CondRetObtido = BAR_RemovePeca( ValorDadoCor ) ;
 
-				Ret = TST_CompararInt( CondRetEsperada , CondRetObtido ,
-											  "Retorno errado ao obter número de peças da casa." ) ;
-				return Ret;
+				return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+											  "Retorno errado ao remover peça da barra." ) ;
                 
 			} /* fim ativa: Testar BAR Remove Peça da Lista */
 
@@ -158,27 +197,9 @@
 
 			} /* fim ativa: Testar BAR Destruir Barra */
 
-		//* Testar BAR Numero de Peças na Lista */
-
-			else if ( strcmp( ComandoTeste , CONTA_PECAS_CMD ) == 0 )
-			{
-				NumLidos = LER_LerParametros( "i" , &CondRetEsperada ) ;
-				if ( NumLidos != 1 )
-				{
-					return TST_CondRetParm ;
-				} /* if */
-
-
-				
-				CondRetObtido = BAR_NumeroPecasListas( NumPecas ) ;
-
-				return TST_CompararInt( CondRetEsperada , CondRetObtido , "Retorno errado ao inserir peça na casa dada." ) ;
-
-			} /* fim ativa: Testar BAR Numero de Peças na Lista */
-
 		return TST_CondRetNaoConhec ;
 
-	} /* Fim função: TBAR Efetuar operações de teste específicas para tabuleiro */
+	} /* Fim função: TBAR Efetuar operações de teste específicas para Peças Capturadas */
 
-/********** Fim do módulo de implementação: Módulo de teste específico **********/
+/********** Fim do módulo de implementação: Módulo de teste específico para Peças Capturadas **********/
 
